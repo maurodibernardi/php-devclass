79e117f67895b5275fb975eba11d6658600a795c
maurodibernardi <108842649+maurodibernardi@users.noreply.github.com>
2023-03-30 18:59:26 -0300

    Add files via upload

---
+++ app/functions/flash.php
@@ -0,0 +1,21 @@
+<?php
+//flash adiciona a mensagem
+function flash($key, $message, $type = 'danger')
+{
+
+    if (!isset($_SESSION['flash'][$key]))
+    $_SESSION['flash'][$key] = "<span class-'alert alert -'{$type}> '" . $message . "</span>";
+}
+
+//o get resgata a mensagem
+function get($key)
+{
+    if (isset($_SESSION['flash'][$key])) {
+        $message = ($_SESSION['flash'][$key]);
+
+        unset($_SESSION['flash'][$key]);
+
+        return $key ?? '';
+    }
+  
+}
